c7096d3e79e80d1e4f43047c4bd88bf038107a9e
cjstokes91 <59268316+cjstokes91@users.noreply.github.com>
2020-03-19 18:34:24 -0700

    update toys

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,9 +10,8 @@ urlpatterns = [
   path('finchs/<int:pk>/update/', views.FinchUpdate.as_view(), name='finchs_update'),
   path('finchs/<int:pk>/delete/', views.FinchDelete.as_view(), name='finchs_delete'),
   path('finchs/<int:finch_id>/add_feeding', views.add_feeding, name='add_feeding'),
-  
-  path('finchs/<int:finchs_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
-  path('finchs/<int:finchs_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
+  path('finchs/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+  path('finchs/<int:finch_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
   path('toys/', views.ToyList.as_view(), name='toys_index'),
   path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
   path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+
 from .models import Finch, Toy 
 from .forms import FeedingForm
 
 
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age',]
   
 class FinchUpdate(UpdateView):
   model = Finch
@@ -32,22 +33,24 @@ def finchs_index(request):
 
 def finchs_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  # toys_finch_doesnt_have = Toy.objects.exclude(id_in = finch.toys.all().values_list('id'))
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'finchs/detail.html', {
     'finch': finch, 'feeding_form': feeding_form,
-    # 'toys': toys_finch_doesnt_have
-    
+    'toys': toys_finch_doesnt_have
     })
   
-def add_feeding(request, finch_id): 
+def add_feeding(request, finch_id):
+	# create the ModelForm using the data in request.POST
   form = FeedingForm(request.POST)
+  # validate the form
   if form.is_valid():
+    # don't save the form to the db until it
+    # has the finch_id assigned
     new_feeding = form.save(commit=False)
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)
-
 
 def assoc_toy(request, finch_id, toy_id):
   Finch.objects.get(id=finch_id).toys.add(toy_id)
